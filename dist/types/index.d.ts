c99a28e1f5d20e4c053a08a3c83ec47fac34a76e
ranjansingh04 <ranjan.pratap@devzery.com>
2024-12-18 20:30:15 +0530

    Fixed Issues

---
+++ dist/types/index.d.ts
@@ -0,0 +1,4 @@
+import devzeryMiddleware from './devzeryMiddleware';
+import devzeryFastifyPlugin from './devzeryFastifyPlugin';
+export default devzeryMiddleware;
+export { devzeryFastifyPlugin };
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import devzeryMiddleware from './devzeryMiddleware';
+import devzeryFastifyPlugin from './devzeryFastifyPlugin';
+
+export default devzeryMiddleware;
+export { devzeryFastifyPlugin };
